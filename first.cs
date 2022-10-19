d9a7fbabee9aa7fd80c87b4ccd6a4f6df448c608
baludevelopers <gbalakrishna370@gmail.com>
2022-10-19 17:41:52 +0530

    master

---
+++ first.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace sample
+{
+    class first
+    {
+        static void main(string[] args)
+        {
+            Console.Write("Hello world ");
+            Console.Write("Hello world2 ");
+            Console.WriteLine("Hello world4");
+        }
+    }
+}
---
+++ second.cs
@@ -0,0 +1,26 @@
+using System.Data;
+using System;
+
+namespace variable
+{
+    class second
+    {
+        static void Main(String[] args)
+        {
+           
+            String name = "John";
+            Console.WriteLine(name);
+            
+            int num = 29;
+            Console.WriteLine(num);
+            double desible = 283.373;
+            Console.WriteLine(desible);
+            Char a = 'A';
+            Console.WriteLine(a);
+            bool b = true;
+            Console.WriteLine(b);
+            const int c = 20;
+            Console.WriteLine(c);
+        }
+    }
+}
